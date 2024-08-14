2edf98750265d307e955084979a9b4d2b2b3c021
priyanshu1664 <priyanshusingh1664@gmail.com>
2024-08-14 16:56:15 +0530

    hard Coded Cart Created

--- src/components/MealItem/MealItemForm.jsx
+++ src/components/MealItem/MealItemForm.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import classes from "./MealItem.module.css";
+import classes from "./MealItemForm.module.css";
+import Input from "../UI/Input";
 
 function MealItemForm(props) {
-  return <div></div>;
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button type="submit">+ Add</button>
+    </form>
+  );
 }
 
 export default MealItemForm;
